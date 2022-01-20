430dad378ed332f3fc2ec685867479ab9fa42de8
Anna Heselbarth <annaheselbarth@gmail.com>
2022-01-19 21:37:06 -0500

    Add project files.

---
+++ ElevenNote.MVC2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ElevenNote.MVC2.Startup))]
+namespace ElevenNote.MVC2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
