a1af5bbe3b29d332fd408a84e4f5ac775ecb8906
Anna Heselbarth <annaheselbarth@gmail.com>
2022-01-21 11:24:56 -0500

    tempdata added

--- ElevenNote.MVC2/Controllers/NoteController.cs
+++ ElevenNote.MVC2/Controllers/NoteController.cs
@@ -29,20 +29,28 @@ namespace ElevenNote.MVC2.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create(NoteCreate model)
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid) return View(model);
+
+            var service = CreateNoteService();
+
+            if (service.CreateNote(model));
             {
-                return View(model);
-            }
+                TempData["SaveResult"] = "Your note was created.";
+                return RedirectToAction("Index");
+            };
 
-            var userId = Guid.Parse(User.Identity.GetUserId());
-            var service = new NoteService(userId);
+            ModelState.AddModelError("", "Note could not be created.");
 
-            service.CreateNote(model);
-
-            return RedirectToAction("Index");
-           
+            return View(model);
         }
 
+
+        private NoteService CreateNoteService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new NoteService(userId);
+            return service;
+        }
 
     }
 }
